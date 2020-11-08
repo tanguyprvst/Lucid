4e0418fa3413b33c7545cf8404aa2b145bdf5793
TanguyPrvst <tanguypruvost67@gmail.com>
2020-11-08 16:45:07 +0100

    public path

--- src/app.js
+++ src/app.js
@@ -11,50 +11,76 @@ class App {
         let routes = [];
 
         const directoryPath = __dirname + '/../app/controllers';
+        const publicPath = __dirname + '/../public';
 
-        // get files
-        fs.readdir(directoryPath, function(err, files) {
-            if(err) throw err;
-        
-            // get routes
-            files.forEach(function (file) {
-                let controllerClass = require(`${directoryPath}/${file}`);
-                let controller = new controllerClass();
-                controller.getRoutes().forEach(arrayroute => {
-                    let path = arrayroute[0];
-                    let method = arrayroute[1];
-                    let func = arrayroute[2];
-                    let middleware = undefined;
-                    if(arrayroute.length == 4) middleware = arrayroute[3];
-                    routes.push(new Route(path, method, func, controller, middleware));
-                })
-            });
-            
-            // get route
-            routes.forEach(route => {
-                if(route.isValidUrl(url) && route.method == method){
-                    execute_route = route;
-                }
-            })
+        let mime = new Map([
+            ['html', 'text/html'],
+            ['txt', 'text/plain'],
+            ['css', 'text/css'],
+            ['gif', 'image/gif'],
+            ['jpg', 'image/jpeg'],
+            ['png', 'image/png'],
+            ['svg', 'image/svg+xml'],
+            ['js', 'application/javascript']
+        ]);
 
-            if(execute_route === undefined){
-                console.log('Undefined route');
-                response.error(res, 'Undefined route');
-                return;
+        let ressource = undefined;
+        mime.forEach((value, key) => {
+            if(url.endsWith(key)){
+                ressource = value;
             }
-
-            // get args
-            let args = [];
-            args['post'] = {};
-            if(execute_route.method == 'POST'){
-                args['post'] = JSON.parse(Buffer.concat(body).toString());
-            }
-
-            args['get']  = execute_route.getUrlArgs(url);
-            args['req'] = req;
-
-            execute_route.execute(res, args);
         });
+
+        if(ressource){
+            fs.readFile(publicPath + url, function (err, data) {
+                if (err) throw err;
+                response.custom(res, data, ressource);
+                
+            });
+        }else{
+            // get files
+            fs.readdir(directoryPath, function(err, files) {
+                if(err) throw err;
+                // get routes
+                files.forEach(function (file) {
+                    let controllerClass = require(`${directoryPath}/${file}`);
+                    let controller = new controllerClass();
+                    controller.getRoutes().forEach(arrayroute => {
+                        let path = arrayroute[0];
+                        let method = arrayroute[1];
+                        let func = arrayroute[2];
+                        let middleware = undefined;
+                        if(arrayroute.length == 4) middleware = arrayroute[3];
+                        routes.push(new Route(path, method, func, controller, middleware));
+                    })
+                });
+                
+                // get route
+                routes.forEach(route => {
+                    if(route.isValidUrl(url) && route.method == method){
+                        execute_route = route;
+                    }
+                })
+
+                if(execute_route === undefined){
+                    console.log('Undefined route: ' + url);
+                    response.error(res, 'Undefined route');
+                    return;
+                }
+
+                // get args
+                let args = [];
+                args['post'] = {};
+                if(execute_route.method == 'POST'){
+                    args['post'] = JSON.parse(Buffer.concat(body).toString());
+                }
+
+                args['get']  = execute_route.getUrlArgs(url);
+                args['req'] = req;
+
+                execute_route.execute(res, args);
+            });
+        }
     }
 }
 
--- src/app/response.js
+++ src/app/response.js
@@ -1,13 +1,19 @@
 class Response {
 
     static error(res, err){
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({error: err}));
+        /*res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({error: err}));*/
     }
 
     static json(res, json){
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(json));
+    }
+
+    static custom(res, data, type){
+        res.writeHead(200, {'Content-Type': type});
+        res.write(data);
+        res.end();
     }
 
     static render(res, html){
