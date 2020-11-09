15e05278efc791e770f3cbe87303307ca721cbc3
TanguyPrvst <tanguypruvost67@gmail.com>
2020-11-09 17:00:50 +0100

    HomeController

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -1,5 +1,4 @@
 const Controller = require('../../src/app/controller');
-const DB = require('../../src/database/database');
 const env = require('../../src/config/env');
 
 class ExempleController extends Controller {
@@ -11,10 +10,6 @@ class ExempleController extends Controller {
     }
 
     home(res){
-        DB.table('users').get(data => {
-            console.log(data);
-        });
-        
         env.search('APP_NAME', data => {
             return this.render(res, 'welcome.html', {
                 name: data
